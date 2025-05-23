6ccd3b98a08c4e686379315d423d30b042593efd
Jordi Bruin <jordi@goodsnooze.com>
2025-05-23 10:33:00 +0200

    Initial Commit

---
+++ StoreKit-Examples/StoreKit_ExamplesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoreKit_ExamplesApp.swift
+//  StoreKit-Examples
+//
+//  Created by Jordi Bruin on 23/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct StoreKit_ExamplesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
